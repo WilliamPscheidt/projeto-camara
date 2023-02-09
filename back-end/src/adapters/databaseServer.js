75aae4029586f79e7dcf51db1ad1957de5ad978f
William Pscheidt <williampscheidt@hotmail.com>
2023-02-08 23:06:50 -0300

    Base database connection

---
+++ back-end/src/adapters/databaseServer.js
@@ -0,0 +1,30 @@
+const database = () => {
+    const mysql = require("mysql2")
+
+    const connect = () => {
+        return mysql.createPool({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'camaras',
+            waitForConnections: true,
+            connectionLimit: 100,
+            maxIdle: 10,
+            idleTimeout: 2000,
+            queueLimit: 5
+        })
+    }
+
+    const execute = (query) => {
+        connect().query(query, function (err, rows, fields){
+            return rows
+        })
+    }
+
+
+    return {
+        execute
+    }
+}
+
+module.exports = database()
--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,6 +1,9 @@
 const httpServer = require("./adapters/httpServer")
+const database = require('./adapters/databaseServer')
 
 httpServer.start(3000, "* Server Online")
 httpServer.get("/", (req, res) => {
     res.send("oi");
 })
+
+database.execute(`select * from ${"camaras".toString()}`)
